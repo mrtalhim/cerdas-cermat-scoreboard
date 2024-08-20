59f7b766ca49ae6f6b32896b9afb5ba945c32b40
mrtalhim <mrtalhim@gmail.com>
2024-08-20 10:49:58 +0700

    timer sound effects

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -3,37 +3,46 @@
         <input placeholder="Scoreboard" class="w-full font-bold text-4xl text-center text-pretty bg-transparent">
         <div class="flex flex-row gap-2 items-baseline m-2">
             <button @click="togglePanel" class="bg-gray-800 text-white px-4 py-2 rounded-lg">
-                {{ isPanelOpen ? 'Hide' : 'Setting' }}
+                Setting
             </button>
-            <button @click="addTeam" class="bg-green-600 text-white px-4 py-2 rounded-lg">Add Team</button>
+            <button @click="addTeam"
+                class="bg-green-600 text-white px-4 py-2 rounded-lg transition-all duration-100 hover:scale-105 active:scale-105">Add
+                Team</button>
             <div v-if="!isCountingDown" class="flex gap-2 items-end">
                 <input v-model.number="minutes" type="number" min="0" placeholder="Minutes"
-                    class="w-20 text-black text-center p-2 border rounded-lg">
+                    class="w-20 text-black text-end p-2 border rounded-lg">
                 <span>m</span>
                 <input v-model.number="seconds" type="number" min="0" placeholder="Seconds"
-                    class="w-20 text-black text-center p-2 border rounded-lg">
+                    class="w-20 text-black text-end p-2 border rounded-lg">
                 <span>s</span>
                 <button @click="startCountdown" class="bg-green-600 text-white px-4 py-2 rounded-lg">Timer</button>
             </div>
         </div>
         <!-- Collapsible Panel -->
-        <transition name="fade">
-            <div v-show="isPanelOpen" class="bg-gray-200 p-6 rounded-lg shadow-lg w-full">
-                <h2 class="text-2xl text-black font-bold mb-4">Edit Score</h2>
-                <div class="flex flex-col space-y-4">
-                    <!-- Dynamic Score Buttons -->
-                    <div class="flex flex-wrap gap-4 justify-center">
+        <transition name="fade" class="absolute self-start">
+            <div v-show="isPanelOpen" class="flex flex-col bg-gray-200 p-8 gap-2 rounded-lg shadow-lg">
+                <div class="flex flex-row gap-2 justify-end items-baseline">
+                    <h2 class="text-5xl text-black font-bold mb-4 w-full">Setting</h2>
+                    <button @click="togglePanel" class="absolute bg-gray-800 text-white px-3 py-2 rounded-lg">
+                        &#10006;
+                    </button>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <span class="text-black font-bold">Set Score Button (0 to hide)</span>
+                    <div class="flex flex-wrap gap-4 justify-center w-auto">
                         <input v-model.number="globalScores.score1" type="number"
-                            class="bg-green-500 text-xl text-white text-center p-2 border rounded-lg"
+                            class="bg-green-500 text-xl text-white font-bold text-center p-2 max-w-32 rounded-lg"
                             placeholder="Score Value" />
                         <input v-model.number="globalScores.score2" type="number"
-                            class="bg-blue-500 text-xl text-white text-center p-2 border rounded-lg"
+                            class="bg-blue-500 text-xl text-white font-bold text-center p-2 max-w-32 rounded-lg"
                             placeholder="Score Value" />
                         <input v-model.number="globalScores.score3" type="number"
-                            class="bg-yellow-500 text-xl text-white text-center p-2 border rounded-lg"
+                            class="bg-yellow-500 text-xl text-white font-bold text-center p-2 max-w-32 rounded-lg"
                             placeholder="Score Value" />
                     </div>
                 </div>
+                <button @click="resetAll" class="bg-red-600 text-white text-center p-2 rounded-lg w-auto">Clear
+                    All Teams</button>
             </div>
         </transition>
 
@@ -55,36 +64,39 @@
             ]">
                 <div class="flex flex-row gap-2">
                     <input v-model="team.name" placeholder="Team Name"
-                        class="text-2xl text-gray-950 text-center font-semibold p-2 w-full border rounded-lg uppercase" />
-                    <button class="bg-red-600 text-white px-4 py-2 rounded-lg"
+                        class="text-2xl text-gray-950 text-center font-bold p-2 w-full border rounded-lg uppercase transition-all duration-100" />
+                    <button class="bg-red-600 text-white px-4 rounded-lg transition-all duration-100 hover:scale-105"
                         @click="removeTeam(index)">&#10006;</button>
                 </div>
 
                 <div class="flex flex-row flex-1 justify-between">
                     <transition name="bounce" mode="out-in">
                         <div :key="team.score"
-                            :class="['text-black font-bold flex-1 flex items-center justify-center', scoreClass]">
+                            :class="['text-black font-bold subpixel-antialiased flex-1 flex items-center justify-center', scoreClass]">
                             {{ team.score }}
                         </div>
                     </transition>
 
                     <div class="flex flex-col justify-center gap-2">
-                        <button @click="changeScore(index, globalScores.score1)"
-                            class="bg-green-500 text-white font-bold px-2 py-1 rounded-lg">{{ globalScores.score1 >= 0 ?
-                                '+' : ''
+                        <button v-if="globalScores.score1 !== 0" @click="changeScore(index, globalScores.score1)"
+                            class="bg-green-500 text-white font-bold px-2 py-1 rounded-lg transition-all duration-100 hover:scale-105">{{
+                                globalScores.score1 >= 0 ?
+                                    '+' : ''
                             }}{{
                                 globalScores.score1 }}</button>
 
-                        <button @click="changeScore(index, globalScores.score2)"
-                            class="bg-blue-500 text-white font-bold px-2 py-1 rounded-lg">{{ globalScores.score2 >= 0 ?
-                                '+' : ''
+                        <button v-if="globalScores.score2 !== 0" @click="changeScore(index, globalScores.score2)"
+                            class="bg-blue-500 text-white font-bold px-2 py-1 rounded-lg transition-all duration-100 hover:scale-105">{{
+                                globalScores.score2 >= 0 ?
+                                    '+' : ''
                             }}{{
                                 globalScores.score2 }}</button>
 
-                        <button @click="changeScore(index, globalScores.score3)"
-                            class="bg-yellow-500 text-white font-bold px-2 py-1 rounded-lg">{{ globalScores.score3 >= 0
-                                ?
-                                '+' : ''
+                        <button v-if="globalScores.score3 !== 0" @click="changeScore(index, globalScores.score3)"
+                            class="bg-yellow-500 text-white font-bold px-2 py-1 rounded-lg transition-all duration-100 hover:scale-105">{{
+                                globalScores.score3 >= 0
+                                    ?
+                                    '+' : ''
                             }}{{ globalScores.score3 }}</button>
                     </div>
                 </div>
@@ -100,7 +112,7 @@ export default {
             teams: [],
             globalScores: {
                 score1: 100,
-                score2: -100,
+                score2: -50,
                 score3: 50
             },
             isPanelOpen: true,
@@ -111,7 +123,9 @@ export default {
             originalSeconds: 0,
             isCountingDown: false,
             countdown: null,
-            backgroundColorClass: '' // For background highlighting
+            backgroundColorClass: '', // For background highlighting
+            alarm: new Audio('buzz.wav'),
+            tick: new Audio('tick.wav'),
         };
     },
     computed: {
@@ -166,6 +180,12 @@ export default {
             }, 500);
         },
         startCountdown() {
+            if (!this.minutes) {
+                this.minutes = 0;
+            }
+            if (!this.seconds) {
+                this.seconds = 0;
+            }
             if (this.minutes === 0 && this.seconds === 0) {
                 return;
             }
@@ -173,11 +193,15 @@ export default {
             this.originalMinutes = this.minutes;
             this.originalSeconds = this.seconds;
             this.isCountingDown = true;
+            this.tick.play();
             this.countdown = setInterval(() => {
                 if (this.seconds === 1 && this.minutes === 0) {
+                    this.alarm.play();
                     this.resetCountdown();
                     this.highlightBackground('bg-red-500'); // Highlight background when countdown ends
                 } else {
+                    this.tick.currentTime = 0;
+                    this.tick.play();
                     if (this.seconds === 0) {
                         this.minutes--;
                         this.seconds = 59;
@@ -192,13 +216,19 @@ export default {
             this.isCountingDown = false;
             this.minutes = this.originalMinutes;
             this.seconds = this.originalSeconds;
+        },
+        resetAll() {
+            clearInterval(this.countdown);
+            this.isCountingDown = false;
+            this.minutes = 0;
+            this.seconds = 5;
+            this.teams = [];
         }
     }
 };
 </script>
 
 <style scoped>
-/* Add transition for smooth background color change */
 .fade-enter-active {
     transition: opacity 0.5s ease;
 }
@@ -234,7 +264,7 @@ export default {
         transform: scale(1);
     }
 
-    25% {
+    10% {
         transform: scale(2);
     }
 
